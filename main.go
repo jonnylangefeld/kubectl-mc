9f651e50bc98e3cb6d2230406897e16c55da4e13
Jonny Langefeld <jonny.langefeld@gmail.com>
2021-02-03 22:40:02 -0800

    Add output feature

--- main.go
+++ main.go
@@ -3,18 +3,33 @@ package main
 import (
 	"bufio"
 	"bytes"
+	"encoding/json"
+	"errors"
 	"fmt"
 	"github.com/spf13/cobra"
 	"go.uber.org/zap"
 	"os"
 	"os/exec"
 	"regexp"
+	"sigs.k8s.io/yaml"
 	"strings"
+)
+
+const (
+	YAML = "yaml"
+	JSON = "json"
 )
 
 var (
 	version string
 	logger  *zap.Logger
+	outputs = map[string]bool{
+		YAML: true,
+		JSON: true,
+	}
+
+	ErrUnknownOutput = errors.New(fmt.Sprintf("this output format is unknown. Choose one of %s", outputsString()))
+	ErrCouldntParseOutput = errors.New(fmt.Sprintf("couldn't parse this output. Are you sure your kubectl command allows for json output? Run command with -d to see debug output"))
 )
 
 func main() {
@@ -31,6 +46,7 @@ type MC struct {
 	ListOnly bool
 	MaxProc  int
 	Debug    bool
+	Output   string
 }
 
 // NewMC registers the default mc command
@@ -55,7 +71,10 @@ mc -r dev -l -n '-test-'
 
 # list all pods with label 'app.kubernetes.io/name=audit' in the 'default' namespace from all clusters with 'gke' in the name, but not 'dev'
 # run max 5 processes in parallel and enable debug output
-mc -r gke -n 'dev' -p 5 -d -- get pods -n gatekeeper-system -l app.kubernetes.io/name=audit`,
+mc -r gke -n 'dev' -p 5 -d -- get pods -n gatekeeper-system -l app.kubernetes.io/name=audit
+
+# print the context and the pod names in kube-system using jq
+mc -r kind -o json -- get pods -n kube-system | jq 'keys[] as $k | "\($k) \(.[$k] | .items[].metadata.name)"'`,
 		SilenceUsage: true,
 		Version:      version,
 		RunE: func(cmd *cobra.Command, args []string) error {
@@ -64,6 +83,12 @@ mc -r gke -n 'dev' -p 5 -d -- get pods -n gatekeeper-system -l app.kubernetes.io
 				logger, _ = zap.NewDevelopment()
 			}
 			defer logger.Sync()
+			if mc.Output != "" {
+				if _, ok := outputs[mc.Output]; !ok {
+					return ErrUnknownOutput
+				}
+				args = append(args, "-o", "json")
+			}
 			if len(args) == 0 && !mc.ListOnly {
 				cmd.Usage()
 				return nil
@@ -77,6 +102,7 @@ mc -r gke -n 'dev' -p 5 -d -- get pods -n gatekeeper-system -l app.kubernetes.io
 	cmd.Flags().BoolVarP(&mc.ListOnly, "list-only", "l", mc.ListOnly, "just list the contexts matching the regex. Good for testing your regex")
 	cmd.Flags().IntVarP(&mc.MaxProc, "max-processes", "p", 100, "max amount of parallel kubectl to be exectued. Can be used to limit cpu activity")
 	cmd.Flags().BoolVarP(&mc.Debug, "debug", "d", mc.Debug, "enable debug output")
+	cmd.Flags().StringVarP(&mc.Output, "output", "o", mc.Output, fmt.Sprintf("specify the output format. Useful for parsing with another tool like jq or yq. One of %s", outputsString()))
 
 	return cmd
 }
@@ -115,13 +141,32 @@ func (mc *MC) run(args []string) error {
 		wait <- true
 	}()
 
+	output := map[string]json.RawMessage{}
 	for _, c := range contexts {
 		logger.Debug("waiting for next free spot", zap.String("context", c))
 		<-parallelProc
 		logger.Debug("executing", zap.String("context", c))
-		go do(done, c, args)
+		go do(done, c, output, mc.Output == "", args)
 	}
 	<-wait
+	if mc.Output != "" {
+		logger.Debug("parsing output...")
+		o, err := json.MarshalIndent(output, "", "  ")
+		if err != nil {
+			logger.Debug("failed to parse output", zap.String("retrieved", fmt.Sprintf("%s", output)))
+			return ErrCouldntParseOutput
+		}
+		switch mc.Output {
+		case JSON:
+			fmt.Printf("%s", o)
+		case YAML:
+			o, err := yaml.JSONToYAML(o)
+			if err != nil {
+				return err
+			}
+			fmt.Printf("%s", o)
+		}
+	}
 	logger.Debug("done")
 
 	return nil
@@ -160,25 +205,28 @@ func (mc *MC) listContexts() (contexts []string, err error) {
 }
 
 // do executes a command against kubectl and sends a bool to the done channel when done
-func do(done chan bool, c string, args []string) {
+func do(done chan bool, context string, output map[string]json.RawMessage, writeToStdout bool, args []string) {
 	var localArgs []string
 	var skipContext bool
 	for _, arg := range args {
 		if arg == "--" {
 			// If this is given, we need to insert the context before this arg
-			localArgs = append(localArgs, "--context", c)
+			localArgs = append(localArgs, "--context", context)
 			skipContext = true
 		}
 		localArgs = append(localArgs, arg)
 	}
 	if !skipContext {
-		localArgs = append(localArgs, "--context", c)
+		localArgs = append(localArgs, "--context", context)
 	}
 	stdout, err := kubectl(localArgs)
 	if err != nil {
 		stdout = bytes.NewBuffer([]byte(err.Error()))
 	}
-	fmt.Printf("\n%s\n%s\n%s", c, strings.Repeat("-", len(c)), string(stdout.Bytes()))
+	output[context] = stdout.Bytes()
+	if writeToStdout {
+		fmt.Printf("\n%s\n%s\n%s", context, strings.Repeat("-", len(context)), string(stdout.Bytes()))
+	}
 	done <- true
 }
 
@@ -192,4 +240,12 @@ func kubectl(args []string) (*bytes.Buffer, error) {
 		return nil, fmt.Errorf(strings.Replace(strings.Replace(stderr.String(), "error: ", "", -1), "Error:", "", -1))
 	}
 	return &stdout, nil
+}
+
+func outputsString() string {
+	keys := make([]string, 0, len(outputs))
+	for k := range outputs {
+		keys = append(keys, k)
+	}
+	return strings.Join(keys, "|")
 }
