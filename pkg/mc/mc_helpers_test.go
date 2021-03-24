19f114cf87f9b181568694cd077be5fa6402801e
Jonny <jonny.langefeld@gmail.com>
2021-03-23 23:47:17 -0700

    Add cmd tests (#11)
    The cobra command still needed tests. For that more dependency injection
    was necessary. This significantly increases the coverage
    
    fix #10

---
+++ pkg/mc/mc_helpers_test.go
@@ -0,0 +1,163 @@
+package mc
+
+const (
+	context = "kind-kind"
+)
+
+var (
+	kubectlReturn = []byte(`NAME                                         READY   STATUS    RESTARTS   AGE
+coredns-66bff467f8-4lnsg                     1/1     Running   0          14h
+coredns-66bff467f8-czsf6                     1/1     Running   0          14h
+etcd-kind-control-plane                      1/1     Running   0          14h
+kindnet-j682f                                1/1     Running   0          14h
+kube-apiserver-kind-control-plane            1/1     Running   0          14h
+kube-controller-manager-kind-control-plane   1/1     Running   0          14h
+kube-proxy-trbmh                             1/1     Running   0          14h
+kube-scheduler-kind-control-plane            1/1     Running   0          14h
+`)
+
+	directories       = "bin\nlib\nlocal\nsbin\nshare\n"
+	directoriesReturn = `
+kind-kind1
+----------
+bin
+lib
+local
+sbin
+share
+`
+	directoriesReturn1 = `
+kind-kind
+---------
+bin
+lib
+local
+sbin
+share
+`
+
+	kubectlReturnSA = []byte(`
+{
+    "apiVersion": "v1",
+    "items": [
+        {
+            "apiVersion": "v1",
+            "kind": "ServiceAccount",
+            "metadata": {
+                "creationTimestamp": "2021-03-21T03:59:54Z",
+                "name": "default",
+                "namespace": "default",
+                "resourceVersion": "372",
+                "selfLink": "/api/v1/namespaces/default/serviceaccounts/default",
+                "uid": "2600c99a-e702-461d-89f6-c5e020e91d30"
+            },
+            "secrets": [
+                {
+                    "name": "default-token-6x8kn"
+                }
+            ]
+        }
+    ],
+    "kind": "List",
+    "metadata": {
+        "resourceVersion": "",
+        "selfLink": ""
+    }
+}
+`)
+
+	jsonReturn = `{
+  "kind-kind": {
+    "apiVersion": "v1",
+    "items": [
+      {
+        "apiVersion": "v1",
+        "kind": "ServiceAccount",
+        "metadata": {
+          "creationTimestamp": "2021-03-21T03:59:54Z",
+          "name": "default",
+          "namespace": "default",
+          "resourceVersion": "372",
+          "selfLink": "/api/v1/namespaces/default/serviceaccounts/default",
+          "uid": "2600c99a-e702-461d-89f6-c5e020e91d30"
+        },
+        "secrets": [
+          {
+            "name": "default-token-6x8kn"
+          }
+        ]
+      }
+    ],
+    "kind": "List",
+    "metadata": {
+      "resourceVersion": "",
+      "selfLink": ""
+    }
+  },
+  "kind-kind1": {
+    "apiVersion": "v1",
+    "items": [
+      {
+        "apiVersion": "v1",
+        "kind": "ServiceAccount",
+        "metadata": {
+          "creationTimestamp": "2021-03-21T03:59:54Z",
+          "name": "default",
+          "namespace": "default",
+          "resourceVersion": "372",
+          "selfLink": "/api/v1/namespaces/default/serviceaccounts/default",
+          "uid": "2600c99a-e702-461d-89f6-c5e020e91d30"
+        },
+        "secrets": [
+          {
+            "name": "default-token-6x8kn"
+          }
+        ]
+      }
+    ],
+    "kind": "List",
+    "metadata": {
+      "resourceVersion": "",
+      "selfLink": ""
+    }
+  }
+}`
+
+	yamlReturn = `kind-kind:
+  apiVersion: v1
+  items:
+  - apiVersion: v1
+    kind: ServiceAccount
+    metadata:
+      creationTimestamp: "2021-03-21T03:59:54Z"
+      name: default
+      namespace: default
+      resourceVersion: "372"
+      selfLink: /api/v1/namespaces/default/serviceaccounts/default
+      uid: 2600c99a-e702-461d-89f6-c5e020e91d30
+    secrets:
+    - name: default-token-6x8kn
+  kind: List
+  metadata:
+    resourceVersion: ""
+    selfLink: ""
+kind-kind1:
+  apiVersion: v1
+  items:
+  - apiVersion: v1
+    kind: ServiceAccount
+    metadata:
+      creationTimestamp: "2021-03-21T03:59:54Z"
+      name: default
+      namespace: default
+      resourceVersion: "372"
+      selfLink: /api/v1/namespaces/default/serviceaccounts/default
+      uid: 2600c99a-e702-461d-89f6-c5e020e91d30
+    secrets:
+    - name: default-token-6x8kn
+  kind: List
+  metadata:
+    resourceVersion: ""
+    selfLink: ""
+`
+)
